c859909d1221fbd0fc745693a974d62134d5b88f
= <=>
2016-11-25 00:30:25 -0800

    Add Sleep Records Widget.

--- app/src/main/java/im/hch/sleeprecord/activities/MainActivity.java
+++ app/src/main/java/im/hch/sleeprecord/activities/MainActivity.java
@@ -8,21 +8,33 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import im.hch.sleeprecord.R;
 import im.hch.sleeprecord.activities.records.SleepRecordsActivity;
+import im.hch.sleeprecord.activities.records.SleepRecordsAdapter;
 import im.hch.sleeprecord.activities.settings.SettingsActivity;
+import im.hch.sleeprecord.models.SleepRecord;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private SleepRecordsAdapter sleepRecordsAdapter;
+
     @BindView(R.id.toolbar) Toolbar toolbar;
     @BindView(R.id.drawer_layout) DrawerLayout drawer;
     @BindView(R.id.nav_view) NavigationView navigationView;
+    @BindView(R.id.list_view) ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +50,54 @@ public class MainActivity extends AppCompatActivity
         toggle.syncState();
 
         navigationView.setNavigationItemSelectedListener(this);
+
+        //TODO replace these default data with real data
+        List<Pair<Date, Date>> sleepTimes = new ArrayList<>();
+        Calendar calendar = Calendar.getInstance();
+        Date begin, end;
+
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        begin = calendar.getTime();
+        calendar.set(Calendar.HOUR_OF_DAY, 6);
+        calendar.set(Calendar.MINUTE, 15);
+        end = calendar.getTime();
+        sleepTimes.add(new Pair<Date, Date>(begin, end));
+
+        calendar.set(Calendar.HOUR_OF_DAY, 10);
+        calendar.set(Calendar.MINUTE, 20);
+        begin = calendar.getTime();
+        calendar.set(Calendar.HOUR_OF_DAY, 11);
+        calendar.set(Calendar.MINUTE, 33);
+        end = calendar.getTime();
+        sleepTimes.add(new Pair<Date, Date>(begin, end));
+
+        calendar.set(Calendar.HOUR_OF_DAY, 13);
+        calendar.set(Calendar.MINUTE, 20);
+        begin = calendar.getTime();
+        calendar.set(Calendar.HOUR_OF_DAY, 16);
+        calendar.set(Calendar.MINUTE, 30);
+        end = calendar.getTime();
+        sleepTimes.add(new Pair<Date, Date>(begin, end));
+
+        calendar.set(Calendar.HOUR_OF_DAY, 20);
+        calendar.set(Calendar.MINUTE, 0);
+        begin = calendar.getTime();
+        calendar.set(Calendar.HOUR_OF_DAY, 23);
+        calendar.set(Calendar.MINUTE, 59);
+        end = calendar.getTime();
+        sleepTimes.add(new Pair<Date, Date>(begin, end));
+
+        //test data
+        ArrayList<SleepRecord> records = new ArrayList<>();
+        for (int i = 27; i >= 25; i--) {
+            SleepRecord sleepRecord = new SleepRecord(11, i);
+            sleepRecord.setSleepTimePairs(sleepTimes);
+            records.add(sleepRecord);
+        }
+
+        sleepRecordsAdapter = new SleepRecordsAdapter(this, records);
+        listView.setAdapter(sleepRecordsAdapter);
     }
 
     @Override
