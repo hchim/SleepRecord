da321958ae415c5f46a4b47d344b9e0de80612ba
Hui Chen <hchen229@gmail.com>
2017-06-06 00:52:20 -0700

    1. Update com.android.support lib to v7:25.3.1. 2. Add nginx config for sleepaidenwebapp.

--- app/src/main/java/im/hch/sleeprecord/serviceclients/EndPoints.java
+++ app/src/main/java/im/hch/sleeprecord/serviceclients/EndPoints.java
@@ -1,8 +1,6 @@
 package im.hch.sleeprecord.serviceclients;
 
 public class EndPoints {
-    //Test endpoint
-//    public static final String BASE_ENDPOINT = "http://192.168.0.4:8081";
     public static final String BASE_ENDPOINT = "https://api.sleepaiden.com";
     public static final String METRIC_SERVICE_ENDPOINT = BASE_ENDPOINT + "/metric";
     public static final String APP_INFO_SERVICE_ENDPOINT = BASE_ENDPOINT + "/appinfo";
